c899625b10b1fed75879053f0e7654f8ef5da886
summerlunaa <summerlunaa9@gmail.com>
2022-02-18 03:11:17 +0900

    release: SECRET_KEY, DEBUG 변경

--- besider/settings.py
+++ besider/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv  
+import os   
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)qpbg&7a^arg6y%(wzn1e*7_n-pdnoovut^p2@+w4ln6$(c8bz'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
